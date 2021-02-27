f904e290ab70ea61bf49122ce9c98ab776d063cd
l0x539 <nogues322@gmail.com>
2021-02-27 02:44:19 +0100

    Fixing main page

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,12 @@
 
 
 import { _404 } from "../404";
-import BoardTest from "../pages/board_test";
 import Challenge from "../pages/challenge";
 
 export const paths = [
     
     { path:"/challenge", component: Challenge },
-    { path:"/", component: BoardTest },
+    { path:"/", component: Challenge },
 
 ]
 
